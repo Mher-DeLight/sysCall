c6a36f1fcf710b0c08a0401febdb08bce531ee1b
Mher DeLight <mahirzamil2012@gmail.com>
2026-08-04 11:52:49 +0300

    set up tokenizer structure

--- include/Tokenizer.h
+++ include/Tokenizer.h
@@ -1,6 +1,61 @@
 #pragma once
+#include <string>
+#include <vector>
+
+class SourceLocation {
+public:
+    int row = -1;
+    int column = -1;
+
+    SourceLocation(int r, int c) : row(r), column(c) {}
+};
+
+enum class TokenType {
+    Identifier,
+    Number,
+    String,
+
+    Plus,
+    Minus,
+    Star,
+    Slash,
+
+    Equal,
+    EqualEqual,
+    NotEqual,
+
+    LParen,
+    RParen,
+
+    Colon,
+    Semicolon,
+    Arrow,
+
+    KeywordIf,
+    KeywordElse,
+
+    VartypeInt,
+    VarypeFloat,
+    VartypeString,
+
+    EndOfFile
+};
+
+class Token {
+public:
+    TokenType type;
+    std::string lexeme;
+    SourceLocation location;
+
+    Token(const TokenType type_, const std::string& lexeme_, const SourceLocation& source_location_)
+        : type(type_), lexeme(lexeme_), location(source_location_) {}
+};
 
 class SysCAll_Tokenizer {
-    int nume1;
-    int num2;
+private:
+    std::vector<Token> tokens;
+
+public:
+    void tokenize(const std::string& code);
+    std::vector<Token>& get_tokens();
 };
